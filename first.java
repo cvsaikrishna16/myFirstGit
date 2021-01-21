614cc2f43d8aa3f39a355bf79e5b9fcb44f429e2
cvsaikrishna16 <cvsaikrishna16@gmail.com>
2021-01-21 08:43:02 +0000

    committing modifed file

--- first.java
+++ first.java
@@ -2,7 +2,8 @@
 
 class First{
 	public static void main(String args[]){
-		System.out.println("hello devops");
+		System.out.println("hello devops, modifying the first line");
+		System.out.println("adding second line");
 	}
 }
 
