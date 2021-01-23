65b9c7ad6df0ddeff63cfc1984967011f8e10e2d
cvsaikrishna16 <cvsaikrishna16@gmail.com>
2021-01-23 08:57:03 +0000

    adding second java file

---
+++ second.java
@@ -0,0 +1,10 @@
+class Second{
+
+	public static void main(String args[]){
+		System.out.println("Second java class");
+		int c = 4+5;
+		System.out.println("Successfully printed the addition values"+c );
+	}
+}
+
+
