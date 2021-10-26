a725994ce376235d22a52f26129aed6798f4705d
Saikrishna <cvsaikrishna16@gmail.com>
2021-10-26 18:58:35 +0530

    practice done on 26th October

--- first.java
+++ first.java
@@ -4,6 +4,7 @@ class First{
 	public static void main(String args[]){
 		System.out.println("hello devops, modifying the first line");
 		System.out.println("adding second line");
+		System.out.println("adding third line");
 	}
 }
 
